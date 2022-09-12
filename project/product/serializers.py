4372c0ab6ab761e29dcbb6927ee291105b6baaed
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-09-12 19:07:49 +0530

    Update serializers.py

--- project/product/serializers.py
+++ project/product/serializers.py
@@ -6,21 +6,3 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
-class ProductListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-class ProductIDSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = '__all__'
-        depth = 1
-    def create(self, validated_data):
-        print (validated_data)
-        return Review.objects.create(**validated_data)
