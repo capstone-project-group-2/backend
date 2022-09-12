f20391d3dd67b55675abc4838b8ca5953a898172
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-09-12 19:08:12 +0530

    Update urls.py

--- project/product/urls.py
+++ project/product/urls.py
@@ -3,10 +3,6 @@ from product.views import *
 
 urlpatterns = [
     path('category/',CategoryAPI.as_view()),
-    path('<int:product_id>/reviews/',GetReviewsByProductId.as_view()),
-    path('newreview/',CreateReview.as_view()),
-    path('<int:num>/',Product_FilterAPI.as_view()),
-    path('product/id/<int:id>/',GetProductById.as_view()),
     path('product/',ProductAPI.as_view()),
 
 ]
