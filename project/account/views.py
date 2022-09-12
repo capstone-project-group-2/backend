5f0399cf1469cb8f21dc8456ebdf6d70181c4737
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-09-12 19:05:11 +0530

    Update views.py

--- project/account/views.py
+++ project/account/views.py
@@ -1,33 +1,65 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import viewsets
-from rest_framework import authentication
 from rest_framework import exceptions
-from rest_framework import generics
-
-from .serializers import UserSerializer, RegisterSerializer
-from .models import User
-
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User, Group
 
-class UsersView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+class ListUserView(APIView):
+    def get(self, request):
+        user_list = list(User.objects.filter(groups__name='Shopping users').values())
 
-class RegisterView(generics.CreateAPIView):
-    serializer_class = RegisterSerializer
+        return Response(user_list)
+
+class SignupView(APIView):
+    def post(self , request):
+        username = request.data['username']
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
+        group = Group.objects.get(name='Shopping users')
+        group.user_set.add(user)
+
+        return Response(
+            {
+                "status":"success" ,
+                'user_id' :user.id , 
+            })
 
 class LoginView(APIView):
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
+        username = request.data["username"]
+        password = request.data["password"]
 
         try:
             user = User.objects.get(username=username)
-            if user:
-                if user.password == password:
+            if user.check_password(password):
+                refresh = RefreshToken.for_user(user)
 
+                return Response(
+                    {
+                        "status":"success" ,
+                        'user_id' :user.id , 
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token)
+                    })
+            else:
+                return Response({"error": "Wrong Password" }) 
+
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+
+class ReportLoginView(APIView):
+    def post(self, request):
+        username = request.data["username"]
+        password = request.data["password"]
+
+        try:
+            user = User.objects.get(username=username)
+            if user.groups.filter(name='Report Generator users'):
+                if user.check_password(password):
                     refresh = RefreshToken.for_user(user)
 
                     return Response(
@@ -39,6 +71,8 @@ class LoginView(APIView):
                         })
                 else:
                     return Response({"error": "Wrong Password" }) 
+            else:
+                return Response({"error": "User does not have permission" }) 
 
         except User.DoesNotExist:
             raise exceptions.AuthenticationFailed('No such user')
