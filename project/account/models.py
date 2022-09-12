2eaf12a21a938a2b96c4d1923ad49ede8687b83e
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-09-12 19:04:43 +0530

    Update models.py

--- project/account/models.py
+++ project/account/models.py
@@ -1,27 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from address.models import AddressField
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
-    is_superuser = models.BooleanField('admin status', default=False)
-    is_staff = models.BooleanField('report generator status', default=False)
-    is_customer = models.BooleanField('shopping user status', default=False)
-
-    def __str__(self):
-        return self.username
-
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    mobile = PhoneNumberField(null=False, blank=False, unique=True, verbose_name='Mobile Phone number')
+    mobile = PhoneNumberField(null=False, blank=False, unique=True)
     avatar = models.ImageField()
 
     def __str__(self):
         return self.user.username
 
 class Address(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, verbose_name='Full name')
-    mobile = PhoneNumberField(null=False, blank=False, unique=True, verbose_name='Mobile number')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    mobile = PhoneNumberField(null=False, blank=False, unique=True)
     address1 = AddressField()
     address2 = AddressField(related_name='+', blank=True, null=True)    
